b1e1887943a2acd16c1a0c2a8b7830aae59c51d5
soka-masa <68989839+soka-masa@users.noreply.github.com>
2020-07-30 23:37:23 +0900

    Add files via upload

---
+++ js/a.js
@@ -0,0 +1,10 @@
+$('#Club1').click(function() {
+    
+    $('#ABT').fadeToggle();
+});
+
+$('#Club2').click(function() {
+    
+    $('#Astronomy1').fadeToggle();
+    $('#Astronomy2').fadeToggle();
+});
